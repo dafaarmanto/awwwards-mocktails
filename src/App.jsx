3a8e7ef962bfb8d936ce9b2b67165bb487cdf432
Dafa Armanto <randikadafa07@gmail.com>
2025-07-03 17:03:26 +0700

    implement navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 
+import Navbar from "./components/Navbar";
+
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-9xl text-zinc-800">Hello GSAP</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 };
 export default App;
